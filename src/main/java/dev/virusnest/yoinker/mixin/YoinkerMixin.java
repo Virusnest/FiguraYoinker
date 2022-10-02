e3fbedcc891390387aa970339f0143c6f85d9ffc
Virusnest <kai.maloney.au@gmail.com>
2022-10-02 16:49:39 +1100

    Work

--- src/main/java/dev/virusnest/yoinker/mixin/YoinkerMixin.java
+++ src/main/java/dev/virusnest/yoinker/mixin/YoinkerMixin.java
@@ -11,6 +11,7 @@ import org.moon.figura.gui.widgets.TextField;
 import org.moon.figura.gui.widgets.TexturedButton;
 import org.moon.figura.gui.widgets.lists.PlayerList;
 import org.moon.figura.gui.widgets.trust.PlayerElement;
+import org.moon.figura.utils.FiguraText;
 import org.spongepowered.asm.mixin.Mixin;
 import org.moon.figura.gui.screens.TrustScreen;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,46 +25,48 @@ import java.util.UUID;
 public class YoinkerMixin extends Screen{
 
 	@Shadow private TextField uuid;
-	@Shadow private TexturedButton back;
+	@Shadow private TexturedButton yoink;
 	@Shadow private PlayerList playerList;
+	@Shadow private TexturedButton back;
 	int middle = this.width / 2;
 	int listWidth = Math.min(middle - 6, 208);
-	@Shadow private TexturedButton yoink = new TexturedButton(middle + listWidth - 18, back.y - 24, 20, 20, Text.literal("yoink"), Text.literal("Set the selected player's avatar"), button -> {
-		String text = uuid.getField().getText();
-		UUID id;
-
-		try {
-			id = UUID.fromString(text);
-		} catch (Exception ignored) {
-			id = FiguraMod.playerNameToUUID(text);
-		}
-
-		if (id == null) {
-			FiguraToast.sendToast("oopsie", FiguraToast.ToastType.ERROR);
-			return;
-		}
-
-		Avatar avatar = AvatarManager.getAvatarForPlayer(id);
-		if (avatar == null || avatar.nbt == null)
-			return;
-
-		if (playerList.selectedEntry instanceof PlayerElement player) {
-			UUID target = player.getOwner();
-			if (FiguraMod.isLocal(target))
-				AvatarManager.localUploaded = false;
-
-			AvatarManager.setAvatar(target, avatar.nbt);
-			LocalAvatarLoader.saveNbt(avatar.nbt);
-			FiguraToast.sendToast("yoinked");
-		}
-	});
-
 	protected YoinkerMixin(Text title) {
 		super(title);
 	}
 
 	@Inject(method = "init()V", at = @At("TAIL"))
 	private void init(CallbackInfo ci) {
+
+		yoink = new TexturedButton(middle + listWidth - 18, back.y - 24, 20, 20, Text.literal("yoink"), Text.literal("Set the selected player's avatar"), button -> {
+			String text = uuid.getField().getText();
+			UUID id;
+
+			try {
+				id = UUID.fromString(text);
+			} catch (Exception ignored) {
+				id = FiguraMod.playerNameToUUID(text);
+			}
+
+			if (id == null) {
+				FiguraToast.sendToast("oopsie", FiguraToast.ToastType.ERROR);
+				return;
+			}
+
+			Avatar avatar = AvatarManager.getAvatarForPlayer(id);
+			if (avatar == null || avatar.nbt == null)
+				return;
+
+			if (playerList.selectedEntry instanceof PlayerElement player) {
+				UUID target = player.getOwner();
+				if (FiguraMod.isLocal(target))
+					AvatarManager.localUploaded = false;
+
+				AvatarManager.setAvatar(target, avatar.nbt);
+				LocalAvatarLoader.saveNbt(avatar.nbt);
+				FiguraToast.sendToast("yoinked");
+			}
+		});
+
 		addDrawableChild(uuid);
 		addDrawableChild(yoink);
 
